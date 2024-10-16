0b0902b9e76f133ddc2f3040f343de792bec2b5b
krauseinafrica <krauseinafrica@icloud.com>
2024-10-16 14:02:38 -0400

    test version

--- studentsrsvp.py
+++ studentsrsvp.py
@@ -1,57 +1,226 @@
+import pandasql as psql
 import streamlit as st
-import pandas as pd
-import gspread
-from datetime import datetime, timedelta
-from streamlit_gsheets import GSheetsConnection
 
-# Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
+st.subheader("📗 Google Sheets st.connection using Service Account")
+
+st.write("#### 1. API Reference")
+with st.echo():
+    import streamlit as st
+
+    from streamlit_gsheets import GSheetsConnection
+
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    st.write(conn)
+    st.help(conn)
+
+docs_url = (
+    "https://docs.streamlit.io/streamlit-community-cloud/get-started/"
+    "deploy-an-app/connect-to-data-sources/secrets-management"
+)
+
+st.write("#### 2. Initial setup")
+st.markdown(
+    f"""
+## Initial setup for CRUD mode
+
+1. Setup `.streamlit/secrets.toml` inside your Streamlit app root directory,
+check out [Secret management documentation]({docs_url}) for references.
+
+2. [Enable API Access for a
+Project](https://docs.gspread.org/en/v5.7.1/oauth2.html#enable-api-access-for-a-project)
+    * Head to [Google Developers Console](https://console.developers.google.com/) and
+      create a new project (or select the one you already have).
+    * In the box labeled “Search for APIs and Services”, search for “Google Drive API”
+      and enable it.
+    * In the box labeled “Search for APIs and Services”, search for “Google Sheets API”
+      and enable it.
+3. [Using Service
+   Account](https://docs.gspread.org/en/v5.7.1/oauth2.html#for-bots-using-service-account)
+    * Enable API Access for a Project if you haven't done it yet.
+    * Go to “APIs & Services > Credentials” and choose “Create credentials > Service
+      account key”.
+    * Fill out the form
+    * Click “Create” and “Done”.
+    * Press “Manage service accounts” above Service Accounts.
+    * Press on ⋮ near recently created service account and select “Manage keys” and then
+      click on “ADD KEY > Create new key”.
+    * Select JSON key type and press “Create”.
+
+You will automatically download a JSON file with credentials. It may look like this:
+```
+{{
+    "type": "service_account",
+    "project_id": "api-project-XXX",
+    "private_key_id": "2cd … ba4",
+    "private_key": "-----BEGIN PRIVATE KEY-----\\nNrDyLw … jINQh/9\\n-----END PRIVATE KEY-----\\n",
+    "client_email": "473000000000-yoursisdifferent@developer.gserviceaccount.com",
+    "client_id": "473 … hd.apps.googleusercontent.com",
+    ...
+}}
+```
+Remember the path to the downloaded credentials file. Also, in the next step you'll need
+the value of client_email from this file.
+
+* **:red[Very important!]** Go to your
+spreadsheet and share it with a client_email from the step above. Just like you do with
+any other Google account. If you don't do this, you'll get a
+`gspread.exceptions.SpreadsheetNotFound` exception when trying to access this
+spreadsheet from your application or a script.
+
+4. Inside `streamlit/secrets.toml` place `service_account` configuration from downloaded
+   JSON file, in the following format (where `gsheets` is your `st.connection` name):
+
+```
+# .streamlit/secrets.toml
+[connections.gsheets]
+spreadsheet = "<spreadsheet-name-or-url>"
+
+# worksheet GID is used when using Public Spreadsheet URL, when using service_account
+# it will be picked as folder_id
+worksheet = "<worksheet-gid-or-folder-id>"
+
+# leave empty when using Public Spreadsheet URL, when using
+# service_account -> type = "service_account"
+type = ""
+
+project_id = ""
+private_key_id = ""
+private_key = ""
+client_email = ""
+client_id = ""
+auth_uri = ""
+token_uri = ""
+auth_provider_x509_cert_url = ""
+client_x509_cert_url = ""
+```
+
+"""
+)
+
+st.write("#### 3. Load DataFrame into Google Sheets")
+
+with st.echo():
+    import streamlit as st
+
+    from streamlit_gsheets import GSheetsConnection
+
+    # Create GSheets connection
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    # Demo Births DataFrame
+    df = psql.load_births()
+
+    # click button to update worksheet
+    # This is behind a button to avoid exceeding Google API Quota
+    if st.button("Create new worksheet"):
+        df = conn.create(
+            worksheet="Example 1",
+            data=df,
+        )
+        st.cache_data.clear()
+        st.rerun()
+
+    # Display our Spreadsheet as st.dataframe
+    st.dataframe(df.head(10))
 
 
-# Constants: Public Google Sheet link and worksheet setup
-SHEET_URL = "https://docs.google.com/spreadsheets/d/12JIjGKy0TUTHO8HmnSc88JhtutxONOblF94Q5D41KRs"
-gc = gspread.service_account()  # Requires Google Sheets API service account JSON in your project directory
-worksheet = gc.open_by_url(SHEET_URL).sheet1  # Access the first worksheet
+st.write("#### 4. Read Google WorkSheet as DataFrame")
+st.info(
+    "If the sheet has been deleted, press 'Create new worksheet' button above.",
+    icon="ℹ️",  # noqa: RUF001
+)
 
-# Helper function to determine Sunday event window
-def get_event_window():
-    today = datetime.now()
-    last_sunday = today - timedelta(days=today.weekday() + 1) if today.weekday() != 6 else today
-    next_sunday = last_sunday + timedelta(days=7)
-    return last_sunday, next_sunday
+with st.echo():
+    import streamlit as st
 
-last_sunday, next_sunday = get_event_window()
+    from streamlit_gsheets import GSheetsConnection
 
-# Load RSVP data from the Google Sheet into a DataFrame
-data = worksheet.get_all_records()
-df = pd.DataFrame(data)
+    # Create GSheets connection
+    conn = st.connection("gsheets", type=GSheetsConnection)
 
-st.title("RSVP to Sunday Event")
+    # Read Google WorkSheet as DataFrame
+    df = conn.read(
+        worksheet="Example 1",
+        usecols=[
+            0,
+            1,
+        ],  # specify columns which you want to get, comment this out to get all columns
+    )
 
-# RSVP Submission Form
-with st.form("RSVP Form"):
-    name = st.text_input("Name")
-    email = st.text_input("Email")
-    attending = st.selectbox("Will you attend?", ["Yes", "No", "Maybe"])
-    submit = st.form_submit_button("Submit")
+    # Display our Spreadsheet as st.dataframe
+    st.dataframe(df)
 
-    if submit:
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        worksheet.append_row([name, email, attending, timestamp])
-        st.success(f"Thank you, {name}! Your RSVP has been recorded.")
+st.write("#### 5. Update Google WorkSheet using DataFrame")
+with st.echo():
+    import streamlit as st
 
-# Display upcoming event details and RSVPs
-st.header("RSVP List for Upcoming Event")
+    from streamlit_gsheets import GSheetsConnection
 
-# Filter RSVPs for the upcoming Sunday event
-df['Timestamp'] = pd.to_datetime(df['Timestamp'], format="%Y-%m-%d %H:%M:%S")
-upcoming_rsvps = df[(df['Timestamp'] >= last_sunday) & (df['Timestamp'] < next_sunday)]
+    # Create GSheets connection
+    conn = st.connection("gsheets", type=GSheetsConnection)
 
-if not upcoming_rsvps.empty:
-    st.dataframe(upcoming_rsvps[['Name', 'Email', 'Attendance Status']])
-else:
-    st.write("No RSVPs yet for the upcoming event.")
+    # Demo Meat DataFrame
+    df = psql.load_meat()
 
-# Display last Sunday and upcoming Sunday for debugging purposes
-st.sidebar.write(f"Last Sunday: {last_sunday.strftime('%Y-%m-%d')}")
-st.sidebar.write(f"Upcoming Sunday: {next_sunday.strftime('%Y-%m-%d')}")
+    # click button to update worksheet
+    # This is behind a button to avoid exceeding Google API Quota
+    if st.button("Update worksheet"):
+        df = conn.update(
+            worksheet="Example 1",
+            data=df,
+        )
+        st.cache_data.clear()
+        st.rerun()
+
+    # Display our Spreadsheet as st.dataframe
+    st.dataframe(df.head(10))
+
+st.write("#### 6. Query Google WorkSheet with SQL and get results as DataFrame")
+st.info(
+    "Mutation SQL queries are in-memory only and do not results in the Worksheet update.",
+    icon="ℹ️",  # noqa: RUF001
+)
+
+
+with st.echo():
+    import streamlit as st
+
+    from streamlit_gsheets import GSheetsConnection
+
+    # Create GSheets connection
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    # make sure worksheet name is in double quota "", in our case it's "Example 1"
+    # DuckDB SQL dialect is supported
+    sql = 'select * from "Example 1"'
+
+    df = conn.query(sql=sql, ttl=3600)
+
+    # Display our SQL query results as st.dataframe
+    st.dataframe(df.head(10))
+
+st.write("#### 7. Clear/delete worksheet")
+with st.echo():
+    import streamlit as st
+
+    from streamlit_gsheets import GSheetsConnection
+
+    # Create GSheets connection
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    # click button to update worksheet
+    # This is behind a button to avoid exceeding Google API Quota
+    if st.button("Clear worksheet"):
+        conn.clear(worksheet="Example 1")
+        st.info("Worksheet Example 1 Cleared!")
+        st.cache_data.clear()
+        st.rerun()
+
+    # click button to delete worksheet using the underlying gspread API
+    # This is behind a button to avoid exceeding Google API Quota
+    if st.button("Delete worksheet"):
+        spreadsheet = conn.client._open_spreadsheet()  # type: ignore
+        worksheet = spreadsheet.worksheet("Example 1")
+        spreadsheet.del_worksheet(worksheet)
+        st.cache_data.clear()
+        st.rerun()
